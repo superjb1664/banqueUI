75db567502f147a645e2f4ba3a9aa34e0ec40e34
superjb1664 <jbaubry25@gmail.Com>
2022-03-10 16:13:33 +0100

    Add webapp packages

---
+++ src/Form/CompteType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Compte;
+use App\Entity\Utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('refbancaire')
+            ->add('utilisateur', EntityType::class, [
+                // looks for choices from this entity
+                'class' => Utilisateur::class,
+                'choice_label' => 'username',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Compte::class,
+        ]);
+    }
+}
