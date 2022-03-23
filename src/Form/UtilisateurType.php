4c832968ba168a119b30b9d40f11ed2dfcf2f748
superjb1664 <jbaubry25@gmail.Com>
2022-03-23 09:33:42 +0100

    Add webapp packages

---
+++ src/Form/UtilisateurType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UtilisateurType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('roles' ,  ChoiceType::class, [
+                'choices' => [
+                    'Administrateur' => "ROLE_ADMIN",
+                    'Utilisateur' => "ROLE_USER" ],
+                'multiple' => true,
+                    ],
+
+            )
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
